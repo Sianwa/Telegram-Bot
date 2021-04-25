88b3161ba918ad722f7a4bd178e74b499b8de8ce
kelsey makale <sianwamak@gmail.com>
2021-04-25 16:54:38 +0300

    bot now echos all messages

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+process.env.NTBA_FIX_319 = 1;
+const TelegramBot = require('node-telegram-bot-api');
+
 require('dotenv').config();
+const token = process.env.TOKEN;
+
+const bot = new TelegramBot(token, {polling : true})
+
+bot.onText(/\/echo (.+)/, (msg, match) =>{
+    const chatId = msg.chat.id;
+    const resp = match[1];
+
+    bot.sendMessage(chatId, resp)
+});
